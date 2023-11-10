8f35d2597aa577e37e844aaa303728a6209a8155
Hubert_Michael_Seelan <hubertmichaelseelan@gmail.com>
2023-11-10 13:26:07 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ const newUserData = async (decodeValue, req, res) => {
     imageURL: decodeValue.picture,
     user_id: decodeValue.user_id,
     email_verfied: decodeValue.email_verified,
-    role: "member",
+    role: "admin",
     auth_time: decodeValue.auth_time,
   });
   try {
